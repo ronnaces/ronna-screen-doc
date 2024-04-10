9bf931a80ac3dbf92558b6ab00db6b6704342c2c
kunlong-luo <luokunlong.loong@gmail.com>
2024-04-10 21:24:45 +0800

    chore: release 2.0.0 version

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,227 @@
+const baiduCode = require('./config/baiduCode.js'); // 百度统计hm码
+const htmlModules = require('./config/htmlModules.js');
+
+
+module.exports = {
+
+  theme: 'vdoing', // 使用依赖包主题
+  // theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/ronnaces/ronna-screen-doc)
+
+  title: "ronna-screen-doc",
+  description: '一个基于VuePress的 知识管理&博客 主题',
+  base: '/ronna-screen-doc/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    ['meta', { name: 'keywords', content: 'vuepress,theme,blog,ronnace,ronna-screen' }],
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+
+    // ['meta', { name: 'wwads-cn-verify', content: '6c4b761a28b734fe93831e3fb400ce87' }], // 广告相关，你可以去掉
+    // ['script', { src: 'https://cdn.wwads.cn/js/makemoney.js', type: 'text/javascript' }], // 广告相关，你可以去掉
+  ],
+
+  // 主题配置
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: "指南",
+        link: "/pages/introduction/",
+        items: [
+          { text: "介绍", link: "/pages/introduction/" },
+          { text: "快速开始", link: "/pages/start/" },
+          { text: "目录结构", link: "/pages/directory/" },
+          { text: "vscode文件夹详解", link: "/pages/vscode/" },
+          { text: "http请求", link: "/pages/request/" },
+          { text: "打包和部署", link: "/pages/build/" },
+          {
+            text: "进阶",
+            items: [
+              { text: "图标", link: "/pages/icon/" },
+              { text: "类型声明", link: "/pages/typescript/" },
+              { text: "打包优化", link: "/pages/buildgood/" },
+              { text: "vite预构建", link: "/pages/optimize/" },
+            ],
+          },
+          {
+            text: "其他",
+            items: [
+              { text: "常见问题", link: "/pages/FAQ/" },
+              { text: "非平台问题跟踪记录", link: "/pages/track/" },
+              { text: "问题反馈", link: "/pages/support/" },
+              { text: "git常用命令", link: "/pages/git/" },
+              { text: "技术网站推荐", link: "/pages/recommendation/" },
+            ],
+          },
+        ],
+      },
+      { text: '资源', link: '/pages/db78e2/' },
+      {
+        text: "日志",
+        items: [
+          {
+            text: "Github日志",
+            link: "https://github.com/ronnaces/ronna-screen/releases",
+          },
+        ],
+      },
+      {
+        text: "优质服务",
+        link: "/pages/service/",
+        icon: `<svg t="1668145036658" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20"
+          height="20">
+          <path
+            d="M428.6976 107.3152c-6.5024 72.192-36.352 207.2576-160.256 337.408 3.6864-48.0256-7.1168-83.7632-19.0464-107.6736-6.6048-13.1584-26.0608-10.5984-28.8768 3.84-5.7344 29.44-20.5824 75.0592-57.6 137.7792-71.6288 121.3952-62.5664 459.8784 340.736 459.8784s430.4384-352.8192 373.1456-496.0256c-37.376-93.44-93.952-152.5248-128.8192-182.3232-11.4176-9.7792-29.1328-1.9456-29.5936 13.056-0.9216 30.464-7.3216 73.3696-33.0752 102.144-0.6656-52.7872-38.144-208.384-202.4448-296.8576-23.296-12.544-51.7632 2.4576-54.1696 28.7744z"
+            fill="#FF5D50" p-id="3479"></path>
+          <path
+            d="M702.2592 678.4c-4.1984-45.056-60.672-166.5536-212.6336-246.4256-10.5984-5.5808-23.0912 3.1232-21.504 15.0016 6.2464 46.848 12.9536 140.4928-24.064 184.7296 4.0448-40.3968-18.1248-73.8304-36.6592-94.3104-8.3968-9.216-23.552-4.6592-25.4976 7.68-3.5328 22.3232-12.8512 56.2688-36.5568 97.9456-42.0864 74.0352-86.9888 188.672 124.5696 294.656 10.9568 0.5632 22.1696 0.8704 33.7408 0.8704 11.2128 0 22.0672-0.3072 32.7168-0.8704 158.2592-59.4944 173.4656-177.9712 165.888-259.2768z"
+            fill="#FFDF99" p-id="3480"></path>
+        </svg>`,
+      },
+      { text: '赞助', link: '/pages/1b12ed/' },
+    ],
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    logo: '/img/logo.png', // 导航栏logo
+    repo: 'ronnaces/ronna-screen-doc', // 导航栏右侧生成Github链接
+    searchMaxSuggestions: 10, // 搜索结果显示最大数
+    lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+
+    // docsDir: 'docs', // 编辑的文件夹
+    // editLinks: true, // 编辑链接
+    // editLinkText: '编辑',
+
+    // 以下配置是Vdoing主题改动的和新增的配置
+    sidebar: { mode: 'structuring', collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+
+    // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
+    updateBar: { // 最近更新栏
+      showToArticle: false, // 显示到文章页底部，默认true
+      // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+    },
+    // titleBadge: false, // 文章标题前的图标是否显示，默认true
+    // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
+    //   '图标地址1',
+    //   '图标地址2'
+    // ],
+
+    pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
+
+    // contentBgStyle: 1,
+
+    category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
+    tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
+    // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
+
+    author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
+      name: 'kunlong-luo', // 必需
+      href: 'https://github.com/ronnaces' // 可选的
+    },
+    social: { // 社交图标，显示于博主信息栏和页脚栏
+      // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
+      icons: [
+        {
+          iconClass: 'icon-github',
+          title: 'GitHub',
+          link: 'https://github.com/kunlong-luo'
+        },
+        {
+          iconClass: 'icon-gitee',
+          title: 'Gitee',
+          link: 'https://gitee.com/kunlong-luo'
+        }
+      ]
+    },
+    footer: { // 页脚信息
+      createYear: 2018, // 博客创建年份
+      copyrightInfo: 'kunlong-luo | MIT License', // 博客版权信息，支持a标签
+    },
+    htmlModules,
+  },
+
+  // 插件
+  plugins: [
+    // [require('./plugins/love-me'), { // 鼠标点击爱心特效
+    //   color: '#11a8cd', // 爱心颜色，默认随机色
+    //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
+    // }],
+
+    ['fulltext-search'], // 全文搜索
+
+    // ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+    //   thirdparty: [ // 可选，默认 []
+    //     {
+    //       title: '在GitHub中搜索',
+    //       frontUrl: 'https://github.com/search?q=', // 搜索链接的前面部分
+    //       behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
+    //     },
+    //     {
+    //       title: '在npm中搜索',
+    //       frontUrl: 'https://www.npmjs.com/search?q=',
+    //     },
+    //     {
+    //       title: '在Bing中搜索',
+    //       frontUrl: 'https://cn.bing.com/search?q='
+    //     }
+    //   ]
+    // }],
+
+    [
+      'vuepress-plugin-baidu-tongji', // 百度统计
+      {
+        hm: baiduCode || '01293bffa6c3962016c08ba685c79d78'
+      }
+    ],
+
+    ['one-click-copy', { // 代码块复制按钮
+      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+      duration: 1000, // prompt message display time.
+      showInMobile: false // whether to display on the mobile side, default: false.
+    }],
+    ['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+      settings: {
+        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+        // cssLib: ['http://xxx'], // 在线示例中的css依赖
+        // vue: 'https://jsd.cdn.zzko.cn/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+        jsfiddle: false, // 是否显示 jsfiddle 链接
+        codepen: true, // 是否显示 codepen 链接
+        horizontal: false // 是否展示为横向样式
+      }
+    }],
+    [
+      'vuepress-plugin-zooming', // 放大图片
+      {
+        selector: '.theme-vdoing-content img:not(.no-zoom)',
+        options: {
+          bgColor: 'rgba(0,0,0,0.6)'
+        },
+      },
+    ],
+    [
+      '@vuepress/last-updated', // "上次更新"时间格式
+      {
+        transformer: (timestamp, lang) => {
+          const dayjs = require('dayjs') // https://day.js.org/
+          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+        },
+      }
+    ]
+  ],
+
+  markdown: {
+    // lineNumbers: true,
+    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+  },
+
+  // 监听文件变化并重新构建
+  extraWatchFiles: [
+    '.vuepress/config.js',
+    '.vuepress/config/htmlModules.js',
+  ],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@alias": "/docs/.vuepress/public",
+      },
+    },
+  },
+}
---
+++ docs/index.md
@@ -0,0 +1,52 @@
+---
+home: true
+heroImage: /img/logo.png
+heroText: ronna-screen-doc
+tagline: 🚀一款简洁美观的 ECharts 开箱即用的数据大屏
+
+actionText: 开始使用 →
+actionLink: /pages/introduction/
+bannerBg: none # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
+
+features: # 可选的
+  - title: 💡 保持稳定的同时采用最新技术栈
+    details: 基于Vue3、Vite、Element-Plus、Echarts、TypeScript、Pinia、Tailwindcss等最新技术栈开发
+  - title: ⚡️ 轻快热重载完善的打包优化方案
+    details: 无论应用程序大小如何，都始终极快的模块热重载（HMR），内置完善的打包优化方案
+  - title: 👋 性能优越运行性能显著
+    details: 项目运行性能显著，开启brotli压缩和cdn替换本地库模式后，打包大小低于2M
+
+# 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
+postList: none
+---
+<p align="center">
+  <a class="become-sponsor" href="/pages/1b12ed/">支持这个项目</a>
+</p>
+
+<style>
+.become-sponsor {
+  padding: 8px 20px;
+  display: inline-block;
+  color: #11a8cd;
+  border-radius: 30px;
+  box-sizing: border-box;
+  border: 1px solid #11a8cd;
+}
+</style>
+
+## ⚡ 反馈与交流
+
+在使用过程中有任何问题和想法，请给我提 [Issue](https://github.com/ronnaces/ronna-screen/issues)。
+你也可以在Issue查看别人提的问题和给出解决方案。
+
+或者加入我们的交流群：
+
+<table>
+  <tbody>
+    <tr>
+      <td align="center" valign="middle">
+        <img :src="$withBase('/img/qrcode/QQ.png')" alt="群号: 891207826" class="no-zoom" style="width:300px;margin: 10px;">
+      </td>
+    </tr>
+  </tbody>
+</table>
